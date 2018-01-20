61a5fe47fbd7838db46525b9b0cbbcc9390dd4a8
jupmasalamanca <jupmasalamanca@gmail.com>
2018-01-19 22:57:17 -0600

    clases anchia

---
+++ Web/Models/MedioPago.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    [Table("xml_medio_pago")]
+    public class MedioPago
+    {
+        [Key]
+        [Required]
+        [MaxLength(3, ErrorMessage = "La propiedad {0} no puede tener más de {1} elementos")]
+        [Display(Name = "Código")]
+        public String codigo { set; get; }
+
+        [Required]
+        [MaxLength(50, ErrorMessage = "La propiedad {0} no puede tener más de {1} elementos")]
+        [Display(Name = "Descripción")]
+        public String descripcion { set; get; }
+
+
+        [Display(Name = "Usuario Creación")]
+        public String usuarioCreacion { set; get; }
+
+        [Display(Name = "Fecha Creación")]
+        public String fechaCreacion { set; get; }
+
+        [Display(Name = "Usuario Modificación")]
+        public String usuarioModificacion { set; get; }
+
+        [Display(Name = "Fecha Modificación")]
+        public String fechaModificacion { set; get; }
+
+        [Display(Name = "Estado")]
+        public String estado { set; get; }
+
+
+    }
+}
---
+++ Web/Models/UnidadMedida.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    [Table("xml_unidad_medida")]
+    public class UnidadMedida
+    {
+        [Key]
+        [Required]
+        [MaxLength(3, ErrorMessage = "La propiedad {0} no puede tener más de {1} elementos")]
+        [Display(Name = "Código")]
+        public String codigo { set; get; }
+
+        [Required]
+        [MaxLength(50, ErrorMessage = "La propiedad {0} no puede tener más de {1} elementos")]
+        [Display(Name = "Descripción")]
+        public String descripcion { set; get; }
+
+
+        [Display(Name = "Usuario Creación")]
+        public String usuarioCreacion { set; get; }
+
+        [Display(Name = "Fecha Creación")]
+        public String fechaCreacion { set; get; }
+
+        [Display(Name = "Usuario Modificación")]
+        public String usuarioModificacion { set; get; }
+
+        [Display(Name = "Fecha Modificación")]
+        public String fechaModificacion { set; get; }
+
+        [Display(Name = "Estado")]
+        public String estado { set; get; }
+    }
+}
