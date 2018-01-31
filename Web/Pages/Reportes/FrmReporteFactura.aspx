03f7ce791cbb7ca7e8547e2bde5aea70a432a18f
AlejandroReyes <roswel030@gmail.com>
2018-01-31 14:02:11 -0600

    Ingreso de código QR en Reportes

---
+++ Web/Pages/Reportes/FrmReporteFactura.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Layout.master" AutoEventWireup="true" CodeBehind="FrmReporteFactura.aspx.cs" Inherits="Web.Pages.Reportes.FrmReporteFactura" %>
+
+<%@ Register Assembly="DevExpress.XtraReports.v17.1.Web, Version=17.1.3.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a" Namespace="DevExpress.XtraReports.Web" TagPrefix="dx" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="Content" runat="server">
+
+    <dx:ASPxWebDocumentViewer ID="ASPxWebDocumentViewer1" runat="server"></dx:ASPxWebDocumentViewer>
+</asp:Content>
+
---
+++ Web/Pages/Reportes/FrmReporteFactura.aspx.cs
@@ -0,0 +1,39 @@
+﻿using DevExpress.XtraPrinting;
+using DevExpress.XtraReports.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web.Pages.Reportes
+{
+    public partial class FrmReporteFactura : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            generarReporte();
+        }
+        private void generarReporte()
+        {
+            //ASPxWebDocumentViewer1.Report = CreateReport();
+            //ASPxWebDocumentViewer1.ReportTypeName = "Web.Reportes.RptFactura";
+            ASPxWebDocumentViewer1.OpenReport(CreateReport());
+
+        }
+
+        RptFactura CreateReport()
+        {
+            RptFactura report = new RptFactura();
+            XMLDomain.FacturaElectronica fe = new XMLDomain.FacturaElectronica();
+            fe.iniciarParametros();
+            object dataSource = fe;
+            report.objectDataSource1.DataSource = dataSource;
+            report.codebar.Text = "WWW.GMAIL.COM";
+            report.CreateDocument();
+           return report;
+        }
+    }
+}
